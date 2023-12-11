2eb3ed02be57725c8dcad77f66ac0586e65a3016
Italo Feitosa <italo85199@gmail.com>
2023-12-11 00:44:21 -0300

    feat(errs): improve error package

---
+++ pkg/errs/http.go
@@ -0,0 +1,36 @@
+package errs
+
+import "net/http"
+
+var statusCodeMap = map[Type]int{
+	InternalType:     http.StatusInternalServerError,
+	NotFoundType:     http.StatusNotFound,
+	ValidationType:   http.StatusBadRequest,
+	BusinessRuleType: http.StatusUnprocessableEntity,
+}
+
+func ToHttpError(err error) HttpError {
+	response := HttpError{
+		StatusCode: http.StatusInternalServerError,
+		Error:      Error{Message: "something went wrong"},
+	}
+	asError, ok := AsError(err)
+	if !ok {
+		return response
+	}
+
+	code, ok := statusCodeMap[asError.Type]
+
+	if !ok {
+		return response
+	}
+
+	response.StatusCode = code
+	response.Error = asError
+	return response
+}
+
+type HttpError struct {
+	StatusCode int   `json:"-"`
+	Error      Error `json:"error"`
+}
